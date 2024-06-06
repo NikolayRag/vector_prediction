ce73b27d3d7fb6bde41bc863947b21cbfed2e8be
NikolayRag <thenickra@gmail.com>
2024-06-06 04:06:09 +0300

    Change get BingX data into JSON

--- tests/bingx_getData.py
+++ tests/bingx_getData.py
@@ -1,6 +1,7 @@
 '''
-Get actual trade data as input
+Get actual trade data from BINGX
 '''
+import logging as log
 
 import time
 import requests
@@ -11,13 +12,13 @@ APIURL = "https://open-api.bingx.com"
 APIKEY = ""
 SECRETKEY = ""
 
-def demo():
+def get_kline():
     payload = {}
     path = '/openApi/spot/v2/market/kline'
     method = "GET"
     paramsMap = {
     "symbol": "BTC-USDT",
-    "interval": "1m",
+    "interval": "1h",
     "limit": 1000
 }
     paramsStr = parseParam(paramsMap)
@@ -25,13 +26,13 @@ def demo():
 
 def get_sign(api_secret, payload):
     signature = hmac.new(api_secret.encode("utf-8"), payload.encode("utf-8"), digestmod=sha256).hexdigest()
-    print("sign=" + signature)
+    log.warning("sign=" + signature)
     return signature
 
 
 def send_request(method, path, urlpa, payload):
     url = "%s%s?%s&signature=%s" % (APIURL, path, urlpa, get_sign(SECRETKEY, urlpa))
-    print(url)
+    log.info(url)
     headers = {
         'X-BX-APIKEY': APIKEY,
     }
@@ -47,42 +48,8 @@ def parseParam(paramsMap):
      return paramsStr+"timestamp="+str(int(time.time() * 1000))
 
 
-
-import _fixpath
-from _plotlib import *
-
 import json
-import numpy as np
-from vector_prediction import VectorSignalPredictor
 
-# Initialize the predictor
-n_steps = 20  # Number of previous steps to use for prediction
-predictor = VectorSignalPredictor(n_steps=n_steps, dropout_rate=0.2)
-
-# Generate some synthetic data for demonstration purposes
-jdata = json.loads(demo())
-data = np.array(jdata['data'])
-
-# Train the model
-predictor.fit(data, epochs=20, split_ratio=0.8)
-
-factor = .01
-data = predictor.smooth_data_ema(data, factor/(factor+1.0))
-
-# Predict the next step
-X_new = data[-n_steps:]  # Take the last n_steps vectors
-y_pred, uncertainty = predictor.predict(X_new, 100)
-
-
-# Print prediction and uncertainty
-print(X_new[-5:,:])
-print("Predicted next vector:", y_pred)
-print("Prediction uncertainty (standard deviation):", uncertainty)
-
-# Calculate and print expected error
-expected_error = predictor.expected_error(data)
-print("Expected Mean Absolute Error on Validation Set:", expected_error)
-
-
-plot_vectors(pd.DataFrame(data[:,1:-3]))
-
+jdata = json.loads(get_kline())['data']
+log.warning(f"{len(jdata)} samples ending with\n{jdata[-4:]}")
+print(jdata)
